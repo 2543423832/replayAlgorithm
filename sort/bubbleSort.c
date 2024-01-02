7f37d903a103f1809b0e877e1f341664c61a30ae
azy <2543423832@qq.com>
2024-01-02 16:07:57 +0800

    [新增]快速排序

--- sort/bubbleSort.c
+++ sort/bubbleSort.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-#define BUFFER_SIZE 6
-/*交换两个整数*/
+/* 交换两个整数 */
 int swap(int *val1, int *val2)
 {
     int ret = 0;
@@ -10,48 +9,56 @@ int swap(int *val1, int *val2)
     *val2 = temp;
     return ret;
 }
-/*数组的函数参数会自动弱化为指针*/
-int printArray(int *array, int arraySize)
-{
-    for (int idx = 0; idx < arraySize; idx++)
+
+
+/* 数组做函数参数会自动弱化成指针 */
+int printArray(int *array, int length)
+{   
+    int ret = 0;
+    for (int idx = 0; idx < length; idx++)
     {
         printf("array[%d] = %d\n", idx, array[idx]);
     }
+    return ret;
 }
-/*时间复杂度:0(n^2)*/
+
+
+/* 时间复杂度: O(n^2) */
 void bubbleSort01(int *array, int length)
 {
     for (int end = length; end > 0; end--)
     {
         for (int begin = 1; begin < end; begin++)
         {
-            /*后面的数 比 前面的数要小 叫交换*/
+            /* 后面的数 比 前面的数要小 叫交换 */
             if (array[begin] < array[begin - 1])
             {
                 swap(&(array[begin]), &(array[begin - 1]));
-            }
+            } 
+
         }
     }
 }
 
+
+/* 优化1 */
 void bubbleSort02(int *array, int length)
 {
     int sorted = 1;
     for (int end = length; end > 0; end--)
     {
-        /*已经排好序的标志*/
+        /* 已经排好序的标志 */
         sorted = 1;
         for (int begin = 1; begin < end; begin++)
         {
-            /*后面的数 比 前面的数要小 叫交换*/
+            /* 后面的数 比 前面的数要小 叫交换 */
             if (array[begin] < array[begin - 1])
             {
                 swap(&(array[begin]), &(array[begin - 1]));
-                /*数据是没有排好序的*/
+                /* 数据是没有排好序的. */
                 sorted = 0;
-            }
+            } 
         }
-        printf("sorted %d\n", sorted);
         if (sorted == 1)
         {
             break;
@@ -59,37 +66,41 @@ void bubbleSort02(int *array, int length)
     }
 }
 
-/*优化2*/
+/* 优化2 */
 void bubbleSort03(int *array, int length)
 {
-    int sorted = 1;
+    int sortedIndex = 0;
     for (int end = length; end > 0; end--)
     {
-        int sortedIndex = 1;
+        /* 已经排好序的索引 */
+        sortedIndex = 1;
         for (int begin = 1; begin < end; begin++)
         {
-            /*后面的数 比 前面的数要小 叫交换*/
+            /* 后面的数 比 前面的数要小 叫交换 */
             if (array[begin] < array[begin - 1])
             {
                 swap(&(array[begin]), &(array[begin - 1]));
-                /*更新排好序的索引*/
+                /* 更新排好序的索引 */
                 sortedIndex = begin;
             }
         }
-        /*更新*/
+        /* 更新. */
         end = sortedIndex;
     }
 }
+
 int main()
 {
-    int array[BUFFER_SIZE] = {2, 1, 3, 7, 50, 60};
-    int lenth = sizeof(array) / sizeof(array[0]);
+    int array[] = {54, 78, 100, 89, 127, 128};
+    int length = sizeof(array) / sizeof(array[0]);
 
-    // bubbleSort01(array, lenth);
+#if 1
+    // bubbleSort01(array, length);
+    // bubbleSort02(array, length);
+    bubbleSort03(array, length);
 
-    // bubbleSort02(array, lenth);
-    bubbleSort03(array, lenth);
-    printArray(array, lenth);
+    printArray(array, length);
+#endif 
 
     return 0;
 }
